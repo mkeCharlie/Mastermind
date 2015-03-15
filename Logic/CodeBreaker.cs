bcf5f810be18066ce12c6dfcfdc48b49fedc05ab
mkecharlie <charliedevguy@gmail.com>
2015-03-14 22:55:57 -0500

    Pi day geekout results

---
+++ Logic/CodeBreaker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// White feedback == right color & position.
+    /// </summary>
+    public class Codebreaker
+    {
+        private int _lastGuessNumber;
+        private readonly List<Tuple<string, string>> _guesses;
+
+
+
+        public Codebreaker()
+        {
+            _guesses = new List<Tuple<string, string>>();
+        }
+
+        public string Guess(string feedback)
+        {
+            if (_lastGuessNumber > 0)
+            {
+                var lastGuessPair = _guesses[_lastGuessNumber - 1];
+
+                var lastGuess = lastGuessPair.Item1;
+
+                _guesses.Remove(lastGuessPair);
+
+                _guesses.Add(new Tuple<string, string>(lastGuess, feedback));
+            }
+
+            string guess;
+            if (feedback.Length == 4)
+            {
+                if (_lastGuessNumber > 1)
+                {
+                    guess = "GGYY";
+                }
+                else
+                {
+                    guess = "BBWW";
+                }
+            }
+            else
+            {
+                if (_lastGuessNumber == 0)
+                {
+                    guess = "WWBB";
+                }
+                else if (_lastGuessNumber == 1)
+                {
+                    guess = "YYGG";
+                }
+                else
+                {
+                    guess = "BBRR";
+                }
+            }
+
+            _guesses.Add(new Tuple<string, string>(guess, null));
+            _lastGuessNumber++;
+            return guess;
+        }
+    }
+}
---
+++ LogicTests/CodebreakerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace LogicTests
+{
+    [TestClass]
+    public class CodebreakerTests
+    {
+        [TestMethod]
+        public void Guess__FirstGuessIs_WWBB()
+        {
+            var sut = new Codebreaker();
+            var actual = sut.Guess("");
+            const string expected = "WWBB";
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Guess__CodeIsBYGB_SecondGuessIs_YYGG()
+        {
+            var sut = new Codebreaker();
+            Assert.AreEqual("WWBB", sut.Guess(""));
+            var actual = sut.Guess("");
+            const string expected = "YYGG";
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Guess__CodeIsWWWW_GuessWWBB_GuesYYGG_GuessBBRR()
+        {
+            var sut = new Codebreaker();
+            Assert.AreEqual("WWBB", sut.Guess(""));
+            Assert.AreEqual("YYGG", sut.Guess("WW"));
+            var actual = sut.Guess("");
+            const string expected = "BBRR";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Guess__CodeIsBBWW_SecondGuessIs_BBWW()
+        {
+            var sut = new Codebreaker();
+            Assert.AreEqual("WWBB", sut.Guess(""));
+            var actual = sut.Guess("BBBB");
+            const string expected = "BBWW";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Guess__CodeIsGGYY_ThirdGuessIs_GGYY()
+        {
+            var sut = new Codebreaker();
+            Assert.AreEqual("WWBB", sut.Guess(""));
+            Assert.AreEqual("YYGG", sut.Guess(""));
+            var actual = sut.Guess("BBBB");
+            const string expected = "GGYY";
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
